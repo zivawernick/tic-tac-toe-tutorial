f5a04ad884472f85bf4ff78ac02096d7c7467d2f
Ziva Wernick <ziva.wernick@gmail.com>
2023-12-04 21:06:39 +0200

    interactive-button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-function Square({value}) {
-  return <button className="square">{value}</button>
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
-  <>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-  </>
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
   );
 }

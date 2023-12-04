cda6ba9e1bc76f12a1aa73d08a8a06811cb31455
Ziva Wernick <ziva.wernick@gmail.com>
2023-12-04 21:39:31 +0200

    add X and save clicks to board, not by square

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
-function Square({ value }) {
-  function handleClick() {
-    console.log('clicked!');
-  }
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
+import { useState } from 'react';
 
-    >
+function Square({ value, onSquareClick }) {
+
+  return (
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
+
+/*Before "Completing the Game"
+Each Square component maintains a part of the game’s state. To check for a winner in a tic-tac-toe game, the Board would need to somehow know the state of each of the 9 Square components.
+At first, you might guess that the Board needs to “ask” each Square for that Square’s state. 
+- this approach is technically possible in React
+discouraged it because the code becomes difficult to understand, susceptible to bugs, and hard to refactor. 
+Instead, the best approach is to store the game’s state in the parent Board component instead of in each Square. 
+The Board component can tell each Square what to display by passing a prop, like you did when you passed a number to each Square.
+*/
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  /*
+  The handleClick function creates a copy of the squares array (nextSquares) with the JavaScript slice() Array method. Then, handleClick updates the nextSquares array to add X to the first ([0] index) square.
+  */
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+  
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );

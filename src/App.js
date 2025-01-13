e2c3e7a0776342a1a2868736882555e48e1dcb3f
Ziva Wernick <ziva.wernick@gmail.com>
2025-01-13 08:42:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -124,3 +124,12 @@ function calculateWinner(squares){
 
 }
 
+/*Recommended add-ons for React Tutorial
+
+If you have extra time or want to practice your new React skills, here are some ideas for improvements that you could make to the tic-tac-toe game, listed in order of increasing difficulty:
+
+For the current move only, show “You are at move #…” instead of a button.
+Rewrite Board to use two loops to make the squares instead of hardcoding them.
+Add a toggle button that lets you sort the moves in either ascending or descending order.
+When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+Display the location for each move in the format (row, col) in the move history list.*/
